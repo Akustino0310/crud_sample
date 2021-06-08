3310a1eb9cf9cfee6e9389a9ecfdbfc2dbfe4c25
Akustino0310 <niggy2014@yahoo.co.jp>
2021-06-09 00:02:31 +0900

    ページ遷移を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,11 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
-    redirect_to action: :index
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to action: :index
   end
 
   def edit
@@ -25,7 +23,6 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to action: :index
   end
   
   private
